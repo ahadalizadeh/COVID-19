f1e795055c6cb0990ec97facf7e4680e44f2c92f
Ahad Alizadeh <st.alizadeh@gmail.com>
2020-03-07 22:07:31 +0330

    Final edit for data manipulation

--- Forecasting.R
+++ Forecasting.R
@@ -1,12 +1,14 @@
 ### Data Manipulation---------
 source("https://raw.githubusercontent.com/ahadalizadeh/COVID-19/master/Download%20data.R")
 source("https://raw.githubusercontent.com/ahadalizadeh/utility_fun/master/utility_fun.R")
-names(COVID19)[2] <- "Country.Region"
 
 diff.fun <- function(data){
   if(!is.atomic(data)) stop("The input data must be atomic!")
  list(diff(c(0,data)))
 }
+
+# COVID19<- COVID19.main
+names(COVID19)[2] <- "Country.Region"
 
 COVID19.Cumulative <- COVID19  %>% 
   group_by(date,Country.Region ) %>% 
@@ -30,7 +32,6 @@ COVID19.temp<- COVID19.Cumulative  %>%
 for (i in 1:dim(COVID19.temp)[1]){
   d = COVID19.temp[i,]
   n= length(d$Confirmed.Daily[[1]])
-for (j in 1:n) {
  d.temp2 = data.frame(Country.Region = rep(d$Country.Region,n),
              Confirmed.Daily = d$Confirmed.Daily[[1]],
              Deaths.Daily = d$Deaths.Daily[[1]],
@@ -38,13 +39,15 @@ for (j in 1:n) {
              day = d$day[[1]],
              date= d$date[[1]])
   
-} 
+
   if( i == 1) d.main <- d.temp2
   if( i != 1) d.main <- rbind(d.main, d.temp2) 
 }
+d.main <- d.main[with(d.main,order(Country.Region,day)),]
 
  
 COVID19 <-merge(COVID19.Cumulative,d.main, 
                 by.x = c("Country.Region","day","date"), 
                 by.y =  c("Country.Region","day","date"))
 COVID19 <- COVID19[with(COVID19,order(Country.Region,day)),]
+
